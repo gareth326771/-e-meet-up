1e01bcfebc228125033ad9b0fa7be58910bfaaf9
kemal yucetin <kemalyucetin@gmail.com>
2018-11-12 19:42:04 +0000

    last state 12.11.18 with broken meetup_id issue to be solved

--- app/models/meetup.rb
+++ app/models/meetup.rb
@@ -2,10 +2,10 @@ class Meetup < ApplicationRecord
   belongs_to :user
   has_many :attendances
   has_many :reviews, through: :attendance
-  validates :user, presence: true
-  validates :location, presence: true
-  validates :capacity, presence: true, numericallity: true
-  validates :date, presence: true
-  validates :price, presence: true, numericallity: true
-  validates :game, presence: true
+  # validates :user, presence: true
+  # validates :location, presence: true
+  # validates :capacity, presence: true, numericality: true
+  # validates :date, presence: true
+  # validates :price, presence: true, numericality: true
+  # validates :game, presence: true
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Meetup.create(user_id: 1, location: "London", capacity: 3, date: "2018-12-28", description: "test description", price: 20, game: "CS:GO")
