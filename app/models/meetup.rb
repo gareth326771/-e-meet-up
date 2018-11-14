b38254c315e3f25ea641c865716ac33aef4498ca
Michael Eldred <mickyfin10@hotmail.com>
2018-11-14 12:01:25 +0000

    Implemented geocoding to meetups and autofill to location fields

--- app/models/meetup.rb
+++ app/models/meetup.rb
@@ -2,6 +2,8 @@ class Meetup < ApplicationRecord
   belongs_to :user
   has_many :attendances
   has_many :reviews, through: :attendance
+  geocoded_by :location
+  after_validation :geocode, if: :will_save_change_to_location?
   # validates :user, presence: true
   # validates :location, presence: true
   # validates :capacity, presence: true, numericality: true
