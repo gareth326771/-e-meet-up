e40abd085d72eb9dbe8f3326ea92ae8c8ee0115a
Gareth Phillips <gareth326771@hotmail.com>
2018-11-16 14:13:56 +0000

    user profiles done

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_15_212758) do
-
+ActiveRecord::Schema.define(version: 2018_11_16_131748) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -71,6 +70,12 @@ ActiveRecord::Schema.define(version: 2018_11_15_212758) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "photo"
+    t.string "gamer_tags"
+    t.string "location"
+    t.string "games"
+    t.string "console"
+    t.integer "ranking"
+    t.text "bio"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
